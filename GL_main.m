21c9f120bd95baf4609e39a6feb7b6e3df9dd947
prm10 <353345737@qq.com>
2015-09-05 15:21:42 +0800

    solve some bug
    pca之前的矩阵需要转置

--- GL_main.m
+++ GL_main.m
@@ -4,7 +4,7 @@ clc;clear all;close all;
 % [date,data]=get_data_from_sql_server('[GL1].[dbo].[ZCS1]',date_str_begin,date_str_end);
 % save data.mat date data;
 
-% datestr(7.352898758101852e+05,'yyyy-mm-dd HH:MM:SS')
+% datestr(date(45506),'yyyy-mm-dd HH:MM:SS')
 
 load data.mat;
 figure,plot(data);
@@ -16,6 +16,23 @@ M_train=mean(data_train0);
 S_train=std(data_train0);
 
 data_train1=guiyihua(data_train0,M_train,S_train);
-data_test1=guiyihua(data_test0,M_train,S_train);
+data_test1=guiyihua(data_test0,M_train,S_train);%测试集
 
 % figure,plot(1:size(date,1),datenum(date)); %采样时间分布不均匀，周期一个小时
+
+m=2;
+x=data_train1';%训练集
+[s,W,S2]=sfa(x);%训练模型
+y=data_test1';%测试集
+[T1,Te,S1,Se]=sfa_indicater(y,W,S2,m);
+
+figure;
+subplot(2,2,1);
+plot(T1);title('T1');
+subplot(2,2,2);
+plot(Te);title('Te');
+subplot(2,2,3);
+plot(S1);title('S1');
+subplot(2,2,4);
+plot(Se);title('Se');
+
--- sf_main.m
+++ sf_main.m
@@ -10,30 +10,33 @@ z_std=std(z_tr,0,2);
 u_tr=(u_tr-u_mean*ones(1,size(u_tr,2)))./(u_std*ones(1,size(u_tr,2)));
 z_tr=(z_tr-z_mean*ones(1,size(z_tr,2)))./(z_std*ones(1,size(z_tr,2)));
 %测试集
-[u_te,z_te]=loaddata('d00_te');
+[u_te0,z_te0]=loaddata('d00_te');
+[u_te1,z_te1]=loaddata('d08_te');
+u_te=[u_te0 u_te1];
+z_te=[z_te0 z_te1];
 %标准化
 u_te=(u_te-u_mean*ones(1,size(u_te,2)))./(u_std*ones(1,size(u_te,2)));
 z_te=(z_te-z_mean*ones(1,size(z_te,2)))./(z_std*ones(1,size(z_te,2)));
 
 %% sf
-
 m=2;
-x=[u_tr;z_tr];
-[s,W,S2]=sfa(x);
+x=[u_tr;z_tr];%训练集
+[s,W,S2]=sfa(x);%训练模型
+y=[u_te;z_te];%测试集
 % figure;
 % subplot(2,1,1);
 % plot(x(1,:)');
 % subplot(2,1,2);
 % plot(s([1,20],:)');
 
-[T1,Te,S1,Se]=sfa_indicater(x,W,S2,m);
+[T1,Te,S1,Se]=sfa_indicater(y,W,S2,m);
 
 figure;
-subplot(2,2,1);
+subplot(2,2,1);title('T1')
 plot(T1);
-subplot(2,2,2);
+subplot(2,2,2);title('Te')
 plot(Te);
-subplot(2,2,3);
+subplot(2,2,3);title('S1')
 plot(S1);
-subplot(2,2,4);
+subplot(2,2,4);title('Se')
 plot(Se);
