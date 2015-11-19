ca5950cbfd2c20945f38a5c945a0d85d1f3f130d
prm14 <prm@datrain.cn>
2015-11-19 20:05:39 +0800

    a

--- get_and_save_data.m
+++ get_and_save_data.m
@@ -5,23 +5,23 @@ GL=7;
 date_str_begin=datestr([2013,12,01,00,00,00],'yyyy-mm-dd');
 date_str_end=datestr([2014,02,01,00,00,00],'yyyy-mm-dd');
 [date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
-save(strcat('K:\GL_data\',No,'\data.mat'),'date0','data0');
+save(strcat('K:\GL_data\',num2str(No),'\data.mat'),'date0','data0');
 fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
 %% No3. GL1 from 2012-10-20 to 2013-03-20
 No=3;
 GL=1;
-date_str_begin=datestr([2012,10,20,00,00,00],'yyyy-mm-dd');
+date_str_begin=datestr([2012,3,20,00,00,00],'yyyy-mm-dd');
 date_str_end=datestr([2013,03,20,00,00,00],'yyyy-mm-dd');
 [date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
-save(strcat('K:\GL_data\',No,'\data.mat'),'date0','data0');
+save(strcat('K:\GL_data\',num2str(No),'\data.mat'),'date0','data0');
 fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
 %% No5. GL1 from 2012-10-20 to 2013-03-20
-No=3;
-GL=1;
-date_str_begin=datestr([2012,10,20,00,00,00],'yyyy-mm-dd');
+No=5;
+GL=5;
+date_str_begin=datestr([2012,05,01,00,00,00],'yyyy-mm-dd');
 date_str_end=datestr([2013,03,20,00,00,00],'yyyy-mm-dd');
 [date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
-save(strcat('K:\GL_data\',No,'\data.mat'),'date0','data0');
+save(strcat('K:\GL_data\',num2str(No),'\data.mat'),'date0','data0');
 fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
 %% fault classification
 % date_str_begin=datestr([2012,03,11,00,00,00],'yyyy-mm-dd');
