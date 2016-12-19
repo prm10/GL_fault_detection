34fe34b21d83039c092288c3f0e6ef2d6989517c
prm10 <353345737@qq.com>
2016-12-19 16:14:59 +0800

    下载2和6号高炉的最新数据

--- get_and_save_data.m
+++ get_and_save_data.m
@@ -1,39 +1,74 @@
 clc;clear;close all;
-%% new No6. GL6 from 2013-07-04 to 2015-12-10
-% No=6;
-% GL=6;
-% date_str_begin=datestr([2013,07,04,00,00,00],'yyyy-mm-dd');
-% date_str_end=datestr([2014,12,10,00,00,00],'yyyy-mm-dd');
-% [date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
-% save(strcat('F:\GL_data\',num2str(No),'\data2013.mat'),'date0','data0');
-% fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+%% new No6. GL6 from 2013-07-15 to 2015-12-08
+%
+No=6;
+GL=6;
+date_str_begin='2013-07-15';
+date_str_end='2014-01-01';
+[date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
+save(strcat('..\GL_data\',num2str(No),'\data_',date_str_begin,'_',date_str_end,'.mat'),'date0','data0');
+fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+clear date0 data0;
 
-% No=6;
-% GL=6;
-% date_str_begin=datestr([2014,12,10,00,00,00],'yyyy-mm-dd');
-% % date_str_begin=datestr([2015,09,21,00,00,00],'yyyy-mm-dd');
-% date_str_end=datestr([2015,12,01,00,00,00],'yyyy-mm-dd');
-% [date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
-% save(strcat('F:\GL_data\',num2str(No),'\data2014.mat'),'date0','data0');
-% fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+date_str_begin='2014-01-01';
+date_str_end='2014-07-01';
+[date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
+save(strcat('..\GL_data\',num2str(No),'\data_',date_str_begin,'_',date_str_end,'.mat'),'date0','data0');
+fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+clear date0 data0;
 
-%% new No2. GL7 from 2013-07-04 to 2015-12-10
+date_str_begin='2014-07-01';
+date_str_end='2015-01-01';
+[date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
+save(strcat('..\GL_data\',num2str(No),'\data_',date_str_begin,'_',date_str_end,'.mat'),'date0','data0');
+fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+clear date0 data0;
+
+date_str_begin='2015-01-01';
+date_str_end='2015-07-01';
+[date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
+save(strcat('..\GL_data\',num2str(No),'\data_',date_str_begin,'_',date_str_end,'.mat'),'date0','data0');
+fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+clear date0 data0;
+
+date_str_begin='2015-07-01';
+date_str_end='2015-12-08';
+[date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
+save(strcat('..\GL_data\',num2str(No),'\data_',date_str_begin,'_',date_str_end,'.mat'),'date0','data0');
+fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+clear date0 data0;
+%}
+%% new No2. GL7 from 2014-02-01 to 2015-12-08
 %
 No=2;
 GL=7;
-date_str_begin=datestr([2014,02,01,00,00,00],'yyyy-mm-dd');
-date_str_end=datestr([2014,12,12,00,00,00],'yyyy-mm-dd');
+date_str_begin='2014-02-01';
+date_str_end='2014-07-01';
 [date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
-save(strcat('..\GL_data\',num2str(No),'\data.mat'),'date0','data0');
+save(strcat('..\GL_data\',num2str(No),'\data_',date_str_begin,'_',date_str_end,'.mat'),'date0','data0');
 fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
-%{
-No=2;
-GL=7;
-date_str_begin=datestr([2015,01,01,00,00,00],'yyyy-mm-dd');
-date_str_end=datestr([2015,12,08,00,00,00],'yyyy-mm-dd');
+clear date0 data0;
+
+date_str_begin='2014-07-01';
+date_str_end='2015-01-01';
 [date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
-save(strcat('F:\GL_data\',num2str(No),'\data2015.mat'),'date0','data0');
+save(strcat('..\GL_data\',num2str(No),'\data_',date_str_begin,'_',date_str_end,'.mat'),'date0','data0');
 fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+clear date0 data0;
+
+date_str_begin='2015-01-01';
+date_str_end='2015-07-01';
+[date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
+save(strcat('..\GL_data\',num2str(No),'\data_',date_str_begin,'_',date_str_end,'.mat'),'date0','data0');
+fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+clear date0 data0;
+
+date_str_begin='2015-07-01';
+date_str_end='2015-12-08';
+[date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
+save(strcat('..\GL_data\',num2str(No),'\data_',date_str_begin,'_',date_str_end,'.mat'),'date0','data0');
+fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
+clear date0 data0;
 %}
 
 %{
@@ -56,10 +91,10 @@ fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
 %% No5. GL1 from 2012-10-20 to 2013-03-20
 No=5;
 GL=5;
-date_str_begin=datestr([2012,05,01,00,00,00],'yyyy-mm-dd');
-date_str_end=datestr([2013,03,20,00,00,00],'yyyy-mm-dd');
+date_str_begin=datestr([2012,05,11,00,00,00],'yyyy-mm-dd');
+date_str_end=datestr([2013,03,28,00,00,00],'yyyy-mm-dd');
 [date0,data0]=get_data_from_sql_server(strcat('[GL',num2str(GL),'].[dbo].[ZCS',num2str(GL),']'),date_str_begin,date_str_end);
-save(strcat('K:\GL_data\',num2str(No),'\data.mat'),'date0','data0');
+save(strcat('..\GL_data\',num2str(No),'\data.mat'),'date0','data0');
 fprintf('get data from %s to %s \n',date_str_begin,date_str_end);
 %}
 
